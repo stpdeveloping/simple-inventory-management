d6b54d0ab6b3856fcf329fe94bfb5a3754d35750
Bartłomiej Stasiak <stprodukcja@gmail.com>
2024-09-23 20:20:33 +0200

    fix(getProductValidationSchema): validation for price and stock

--- src/modules/business/product/validation/getProductValidationSchema.ts
+++ src/modules/business/product/validation/getProductValidationSchema.ts
@@ -3,6 +3,8 @@ import vine from "@vinejs/vine";
 export const getProductValidationSchema = () => vine.object({
     name: vine.string().minLength(1).maxLength(50),
     description: vine.string().minLength(1).maxLength(50),
-    price: vine.number({ strict: true }).positive().min(1),
+    price: vine.number({ strict: true }).positive().min(1)
+        .decimal([0, 2]),
     stock: vine.number({ strict: true }).positive()
+        .withoutDecimals()
 });
